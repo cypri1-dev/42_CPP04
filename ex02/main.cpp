ccf082494e902efd3b03c4bc5ecd8acebb79ec22
Cyprien Ferreira <cyferrei@paul-f4Br7s1.clusters.42paris.fr>
2024-11-12 17:59:49 +0100

    [ALL_DONE]: tests OK - no leaks

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:56:03 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/11/12 17:53:11 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:59:43 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,29 @@ int main() {
 	const int size = 10;
 	const Animal* animals[size];
 
-	// Création d'un tableau d'animaux
-		for (int i = 0; i < size; i++) {
-			if (i < size / 2) {
-				animals[i] = new Dog();
-			} else {
-				animals[i] = new Cat();
-			}
+	for (int i = 0; i < size; i++) {
+		if (i < size / 2) {
+			animals[i] = new Dog();
+		} else {
+			animals[i] = new Cat();
 		}
+	}
 
-	// Appel de makeSound pour tous les animaux
 	for (int i = 0; i < size; i++) {
 		std::cout << "Animal " << i + 1 << " : ";
 		animals[i]->makeSound();
 	}
 	
-	// Libération de la mémoire allouée
 	for (int i = 0; i < size; i++) {
 		delete animals[i];
 	}
 
-	// Test de la copie
 	const Dog doggo1;
 	doggo1.setIdeasDog("Woauf", 0);
 	std::cout << "doggo1: " << doggo1.getIdeasDog(0) << std::endl;
 
 	{
-		const Dog doggo2 = doggo1;  // Appel du constructeur de copie
+		const Dog doggo2 = doggo1;
 		std::cout << "doggo2: " << doggo2.getIdeasDog(0) << std::endl;
 		doggo2.setIdeasDog("MOuaff", 0);
 		std::cout << "doggo1 après modification de doggo2: " << doggo1.getIdeasDog(0) << std::endl;
